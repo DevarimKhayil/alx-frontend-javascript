1bd3a0f283f6a7666356901f92a448d8f7ac5966
devarim <wachirabriangeita@gmail.com>
2024-07-25 20:07:16 +0300

    fine tune code

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -1,13 +1,26 @@
+// 5-main.js
 import Building from './5-building.js';
 
-const b = new Building(100);
-console.log(b);
+// Attempting to create an instance of the abstract Building class should throw an error
+try {
+  const b = new Building(100);
+  console.log(b);
+} catch (err) {
+  console.error('Error:', err.message);
+}
 
-class TestBuilding extends Building {}
+// Creating an instance of the derived class that correctly implements evacuationWarningMessage
+class TestBuilding extends Building {
+  evacuationWarningMessage() {
+    return 'TestBuilding evacuation warning!';
+  }
+}
 
 try {
-    new TestBuilding(200)
+  const tb = new TestBuilding(200);
+  console.log(tb);
+  console.log(tb.evacuationWarningMessage());
+} catch (err) {
+  console.error('Error:', err.message);
 }
-catch(err) {
-    console.log(err);
-}
+
